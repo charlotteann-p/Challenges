ea719b8c2dacfd4a3b091e8cacb22312854442c2
Charlotte Powning <capowning@gmail.com>
2019-04-04 20:22:30 -0400

    Updated with move logic

--- dynamicDrawBoard/Program.cs
+++ dynamicDrawBoard/Program.cs
@@ -4,18 +4,28 @@ namespace dynamicDrawBoard
 {
     class Program
     {
-        static void Main()
+        static void Main() 
         {
             Console.WriteLine("Let's make an array! You decide the size.");
             Console.Write("Number of columns: ");
-            int x = Convert.ToInt32(Console.ReadLine());
+            int columns = Convert.ToInt32(Console.ReadLine());
             Console.Write("Number of rows: ");
-            int y = Convert.ToInt32(Console.ReadLine());
-            for (var i = 0; i < y; i++)
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Pick a numbered space for your next move: ");
+            int movelocation = Convert.ToInt32(Console.ReadLine());
+            int counter = 1;
+            for (var i = 0; i < rows; i++)
             {
-                for (var j = 0; j < x; j++)
+                for (var j = 0; j < columns; j++)
                 {
-                    Console.Write("x  ");
+                    string template = @" [{0}] ";
+                    int boxnumber = counter++;
+                    var movespace = String.Format(template, boxnumber);
+                    if (movelocation == boxnumber)
+                    {
+                        movespace = " [x] ";
+                    }
+                    Console.Write(movespace);
                 }
                 Console.WriteLine('\n');
             }
